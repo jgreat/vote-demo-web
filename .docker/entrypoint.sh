cabccbcbf0c2f9a6dcc6bd64c2c300da55ae3871
jgreat <jason.greathouse@leankit.com>
2016-07-21 15:59:34 -0500

    drone.yml

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -16,30 +16,28 @@ if [ "$LK_RANCHER_SERVICE_DISCOVERY" ]; then
   env_vnet=$( echo ${NAMES[0]} )
   env_instance=$( echo ${NAMES[1]} )
   env_name=$( echo $env | jq -r '.name')
+
   # setup all of the environment variable
   if [ -z "$LK_ENVIRONMENT" ]; then
     export LK_ENVIRONMENT=$( echo $env_instance )
   fi
 
-  if [ -z "$LK_AUTH_USER_ENDPOINT_PROTOCOL" ]; then
-    export LK_AUTH_USER_ENDPOINT_PROTOCOL=https
+  if [ -z "$RABBITMQ_HOST" ]; then
+    export RABBITMQ_HOST=rabbit.rabbit.rancher.internal
+  fi
+  if [ -z "$RABBITMQ_PORT" ]; then
+    export RABBITMQ_PORT=$( echo $rabbit | jq -r '.labels["io.leankit.service.port.amqp"]' | sed -e 's/\/\(tcp\|udp\)//' )
+  fi
+  if [ -z "$RABBITMQ_USER" ]; then
+    export RABBITMQ_USER=$( echo $rabbit | jq -r '.labels["io.leankit.service.username"]' )
+  fi
+  if [ -z "$RABBITMQ_PASS" ]; then
+    export RABBITMQ_PASS=$( echo $rabbit | jq -r '.labels["io.leankit.service.password"]' )
+  fi
+  if [ -z "$RABBITMQ_VHOST" ]; then
+    export RABBITMQ_VHOST=$( echo $rabbit | jq -r '.labels["io.leankit.service.vhost"]' )
   fi
 
-  if [ -z "$LK_RABBIT_HOST" ]; then
-    export LK_RABBIT_HOST=rabbit.rabbit.rancher.internal
-  fi
-  if [ -z "$LK_RABBIT_PORT" ]; then
-    export LK_RABBIT_PORT=$( echo $rabbit | jq -r '.labels["io.leankit.service.port.amqp"]' | sed -e 's/\/\(tcp\|udp\)//' )
-  fi
-  if [ -z "$LK_RABBIT_USER" ]; then
-    export LK_RABBIT_USER=$( echo $rabbit | jq -r '.labels["io.leankit.service.username"]' )
-  fi
-  if [ -z "$LK_RABBIT_PASS" ]; then
-    export LK_RABBIT_PASS=$( echo $rabbit | jq -r '.labels["io.leankit.service.password"]' )
-  fi
-  if [ -z "$LK_RABBIT_VHOST" ]; then
-    export LK_RABBIT_VHOST=$( echo $rabbit | jq -r '.labels["io.leankit.service.vhost"]' )
-  fi
 fi
 
 # print out all of the environment variables
