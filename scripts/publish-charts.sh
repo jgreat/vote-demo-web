7171f6ec96bb43b9559c6a67750625fc676f0fb7
Jason Greathouse <jgreat@jgreat.me>
2018-11-23 14:01:30 -0600

    test vars

--- scripts/publish-charts.sh
+++ scripts/publish-charts.sh
@@ -2,33 +2,35 @@
 
 set -e
 
-# Get the first semver tag
-IFS=',' read -ra TAGS <<< $(cat .tags)
-for t in "${TAGS[@]}"; do
-    echo "Found tag: ${t}"
-    if [[ $t =~ [v]*[0-9]+\.[0-9]+\.[0-9]+.* ]]; then
-        VERSION=$t
-        break
-    fi
-done
-echo "VERSION: $VERSION"
+if [ -z "${CHART_VERSION}" ]; then
+    # Get the first semver tag
+    IFS=',' read -ra TAGS <<< $(cat .tags)
+    for t in "${TAGS[@]}"; do
+        echo "Found tag: ${t}"
+        if [[ $t =~ [v]*[0-9]+\.[0-9]+\.[0-9]+.* ]]; then
+            VERSION=$t
+            break
+        fi
+    done
+fi
+echo "CHART_VERSION: $CHART_VERSION"
 
 # If master then its a "release", else its a feature branch.
 # Releases are named with the git repo name
-if [ "${CICD_GIT_BRANCH}" == "master" ]; then
-    echo "Found master branch."
-    CHART_NAME="${CICD_GIT_REPO_NAME}"
-else
-    echo "Found feature branch."
-    CHART_NAME="${CICD_GIT_BRANCH}"
-fi
+# if [ "${CICD_GIT_BRANCH}" == "master" ]; then
+#     echo "Found master branch."
+#     CHART_NAME="${CICD_GIT_REPO_NAME}"
+# else
+#     echo "Found feature branch."
+#     CHART_NAME="${CICD_GIT_BRANCH}"
+# fi
 echo "CHART_NAME: ${CHART_NAME}"
 
 mkdir -p .build/charts
-cp -R ./.chart/${CICD_GIT_REPO_NAME} .build/${CHART_NAME}
+cp -R .chart .build/${CHART_NAME}
 
 # sed replace version and name
-sed -i -e "s/%VERSION%/${VERSION}/g" .build/${CHART_NAME}/Chart.yaml
+sed -i -e "s/%VERSION%/${CHART_VERSION}/g" .build/${CHART_NAME}/Chart.yaml
 sed -i -e "s/%CHART_NAME%/${CHART_NAME}/g" .build/${CHART_NAME}/Chart.yaml
 
 export HELM_HOME=/root/.helm
@@ -41,7 +43,7 @@ helm package -d .build/charts .build/${CHART_NAME}
 
 # Add Remote Repo
 helm repo add --username "${HELM_REPO_USERNAME}" --password "${HELM_REPO_PASSWORD}" \
-${CICD_GIT_REPO_NAME} https://vote-demo-charts.eng.rancher.space/${CICD_GIT_REPO_NAME}/
+${HELM_REPO_NAME} ${HELM_REPO_URL}
 
 # Publish Chart 
-helm push .build/charts/${CHART_NAME}-${VERSION}.tgz ${CICD_GIT_REPO_NAME}
+helm push .build/charts/${CHART_NAME}-${CHART_VERSION}.tgz ${HELM_REPO_NAME}
